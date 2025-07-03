d66d951282cf8c7a20765b6fc7a3b5371dc7dda2
Genmurraya <dermawankemuning@gmail.com>
2025-07-03 22:06:31 +0800

    Selesai

--- Main.java
+++ Main.java
@@ -1,137 +1,178 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main{
-    public static void main(String[] args){
-        int[] numbers = new int[1000]; //inisiasi awal array
-        for (int i =0; i < 1000; i++){
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Masukkan jumlah elemen:");
+        int elemen = scanner.nextInt();
+
+        int[] numbers = new int[elemen]; //inisiasi awal array
+        for (int i = 0; i < elemen; i++) {
             numbers[i] = i + 1;
         }
-        int[] newNumbers = new int[numbers.length+1]; //array baru setelah penyisipan
-
         ArrayList<Integer> numbers2 = new ArrayList<>(); //inisiasi Arraylist
-        for (int i = 0; i < 1000 ; i++){
-            numbers2.add(i+1);
+        for (int i = 0; i < elemen; i++) {
+            numbers2.add(i + 1);
         }
-         //Traversal pada Array
-        long startTimeTraversal = System.nanoTime();
-        System.out.println("Array traversal: " + Arrays.toString((numbers))); //melakukan traversal array
-        long stopTimeTraversal = System.nanoTime();
-        long durationTraversal = stopTimeTraversal - startTimeTraversal;
 
-        //Traversal pada Arraylist
-        long startTimeTraversal2 = System.nanoTime();
-        System.out.println("Arraylist traversal: " + numbers2 + "\n"); //melakukan traversal pada rraylist
-        long stopTimeTraversal2 = System.nanoTime();
-        long durationTraversal2 = stopTimeTraversal2 - startTimeTraversal2;
+        Operation operator = new Operation();
+        Comparison comparer = new Comparison();
 
-        //Pencarian pada array
-        long startTime = System.nanoTime(); //mencatat waktu sebelum operasi pencarian array dimulai
+        System.out.println("Performa Operasi dasar Array\n");
+        long[] arrayDurations = operator.ArrayOperation(numbers);
 
-        int target = 500;
-        boolean found = false;
-        for (int i = 0; i < numbers.length ; i++ )
-            if(numbers[i] == target){
-                System.out.println("Pencarian " + target + " dalam array: ditemukan di indeks " + i);
-                found = true;
-                break;
+        System.out.println("Performa Operasi dasar pada Arraylist\n ");
+        long [] arraylistDuration = operator.ArraylistOperation(numbers2);
+
+        System.out.println("Perbandingan Kedua Operasi\n ");
+        comparer.printComparisonResult(arrayDurations, arraylistDuration);
+
+    }
+}
+class Operation{
+
+        private void printOperationTime(String operation, long duration){
+            System.out.println("Waktu Eksekusi " + operation + ": " + ((float) duration / 1e6 + " ms\n" ));
+        }
+
+        public long[] ArrayOperation(int[] numbers) {
+            long[] durations = new long[5];
+
+            //Traversal pada Array
+            long startTimeTraversal = System.nanoTime();
+            System.out.println("Array traversal: " + Arrays.toString((numbers))); //melakukan traversal array
+            long stopTimeTraversal = System.nanoTime();
+            durations[0] = stopTimeTraversal - startTimeTraversal;
+            printOperationTime("Traversal pada array: ", durations[0]);
+
+            //Pencarian pada array
+            long startTime = System.nanoTime(); //mencatat waktu sebelum operasi pencarian array dimulai
+            int target = numbers.length/2;
+            boolean found = false;
+            for (int i = 0; i < numbers.length; i++)
+                if (numbers[i] == target) {
+                    System.out.println("Pencarian " + target + " dalam array: ditemukan di indeks " + i);
+                    found = true;
+                    break;
+                }
+            if (!found) {
+                System.out.println("Tidak ditemukan!");
             }
+            long stopTime = System.nanoTime(); //mencatat waktu selesai pencarian pada array
+            durations[1] = stopTime - startTime; //menghitung waktu operasi dengan mengurangkan waktu stop dengan selesai
+            printOperationTime("Searching pada array: ", durations[1]);
 
-        long stopTime = System.nanoTime(); //mencatat waktu selesai pencarian pada array
-        long durationPencarian = stopTime - startTime; //menghitung waktu operasi dengan mengurangkan waktu stop dengan selesai
 
-        if(!found){
-            System.out.println("Tidak ditemukan!");
+            // Insertion pada Array
+            long startTimeInsertion = System.nanoTime(); //mencatat waktu mulai operasi
+            int [] newNumbers = new int [numbers.length+1];
+            int insertion = numbers.length+ 1;
+            int pos = numbers.length;
+            System.arraycopy(numbers, 0, newNumbers, 0, pos); //menggunakan arraycopy untuk menyalin bilangan sebelum pos newNumbers
+            newNumbers[pos] = insertion; //menyisipkan bilangan ke newNumbers
+            System.arraycopy(numbers, pos, newNumbers, pos + 1, numbers.length - pos); //menggunakan arraycopy untuk menyalin bilangan setelah bilangan baru
+
+            System.out.println("Array setelah penyisipan: " + Arrays.toString((newNumbers)));
+            long stopTimeInsertion = System.nanoTime(); //mencatat waktu selesia operasi
+            durations[2] = stopTimeInsertion - startTimeInsertion; //menghitung waktu operasi
+            printOperationTime("Penyisipan pada array: ", durations[2]);
+
+
+            //Delete pada Array
+            long startTimeDeletion = System.nanoTime(); //mencatat waktu awal sebelum mulai
+            int[] deleteNumbers = new int[newNumbers.length - 1]; // total elemen array yang dihapus -1 dari array sebelumnya
+            int indexDeletion = 0;
+            System.arraycopy(newNumbers, 0, deleteNumbers, 0, indexDeletion); //mengcopy isi array sebelum yang ingin dihapus
+            System.arraycopy(newNumbers, indexDeletion + 1, deleteNumbers, indexDeletion, newNumbers.length - 1 - indexDeletion); //mengcopy isi array setelah ingin dihapus
+            System.out.println("Array setelah penghapusan: " + Arrays.toString((deleteNumbers)));
+            long stopTimeDeletion = System.nanoTime(); //mencatat waktu saat selesai
+            durations[3] = stopTimeDeletion - startTimeDeletion; // menghitung lamanya operasi
+            printOperationTime("Delete pada array: ", durations[3]);
+
+            //Reverse pada Array
+            long startTimeReverse = System.nanoTime(); //mencatat waktu mulai
+            int[] reverseNumbers = new int[deleteNumbers.length]; //membuat array baru
+            for (int i = 0; i < deleteNumbers.length; i++) {
+                reverseNumbers[i] = deleteNumbers[deleteNumbers.length - 1 - i]; //array baru nilainya dari kecil ke besar diisi dengna nilai array lama dari besar ke kecil
+            }
+            System.out.println("Array Reverse: " + Arrays.toString((reverseNumbers)));
+            long stopTimeReverse = System.nanoTime(); //mencatat waktu selesai
+            durations[4] = stopTimeReverse - startTimeReverse; //mencatat waktu
+            printOperationTime("Reverse pada array: ",durations[4]);
+
+            return durations;
+
         }
 
 
-        //Pencarian pada Arraylist
-        long startTime2 = System.nanoTime(); //mencatat waktu mulai operasi pencarian array list
 
-        int target2 = 500;
-        int index2 = numbers2.indexOf(target2); //sudah ada built in functionnya
+        public long[] ArraylistOperation(ArrayList<Integer> numbers2) {
+            long[] durations = new long[5];
 
-        System.out.println(("Pencarian " + target2 + " dalam arraylist: ditemukan di indeks " + index2 + "\n"));
-
-        long stopTime2 = System.nanoTime(); // waktu selesai operasi pencarian arrayList
-        long durationPencarian2 = stopTime2 - startTime2; //Menghitung waktu operasi pencarian arraylist
-
-
-        // Insertion pada Array
-        long startTimeInsertion = System.nanoTime();
-        int insertion = 1001;
-        int pos = 1000;
-        System.arraycopy(numbers, 0, newNumbers, 0, pos); //menggunakan arraycopy untuk menyalin bilangan sebelum pos newNumbers
-        newNumbers[pos] = insertion; //menyisipkan bilangan ke newNumbers
-        System.arraycopy(numbers,pos, newNumbers,pos+1,numbers.length - pos); //menggunakan arraycopy
-
-        System.out.println("Array setelah penyisipan: " + Arrays.toString((newNumbers)));
-        long stopTimeInsertion = System.nanoTime();
-        long durationInsertion = stopTimeInsertion - startTimeInsertion;
+            //Traversal pada Arraylist
+            long startTimeTraversal2 = System.nanoTime();
+            System.out.println("Arraylist traversal: " + numbers2); //melakukan traversal pada rraylist
+            long stopTimeTraversal2 = System.nanoTime();
+            durations[0] = stopTimeTraversal2 - startTimeTraversal2;
+            printOperationTime("Tranversal pada arraylist: ",durations[0]);
 
 
-        //Insertion pada Arraylist
-        long startTimeInsertion2 = System.nanoTime();
-        numbers2.add(1000,1001); //sudah ada builtin fuction untuk operasi penyisipan pada arraylist
-        System.out.println("Arraylist setelah penyisipan: " + numbers2 + "\n");
-        long stopTimeInsertion2 = System.nanoTime();
-        long durationInsertion2 = stopTimeInsertion2 - startTimeInsertion2;
+            //Pencarian pada Arraylist
+            long startTime2 = System.nanoTime(); //mencatat waktu mulai operasi pencarian array list
+            int target2 = numbers2.size();
+            int index2 = numbers2.indexOf(target2); //sudah ada built in functionnya
+            System.out.println(("Pencarian " + target2 + " dalam arraylist: ditemukan di indeks " + index2));
+
+            long stopTime2 = System.nanoTime(); // waktu selesai operasi pencarian arrayList
+            durations[1]= stopTime2 - startTime2; //Menghitung waktu operasi pencarian arraylist
+            printOperationTime("Searching pada arraylist: ",durations[1]);
+
+            //Insertion pada Arraylist
+            long startTimeInsertion2 = System.nanoTime();
+            numbers2.add(numbers2.size(), numbers2.size() + 1); //sudah ada builtin fuction untuk operasi penyisipan pada arraylist
+            System.out.println("Arraylist setelah penyisipan: " + numbers2);
+            long stopTimeInsertion2 = System.nanoTime();
+            durations[2] = stopTimeInsertion2 - startTimeInsertion2;
+            printOperationTime("Inserton pada arraylist: ",durations[2]);
+
+            // Delete pada arraylist
+            long startTimeDeletion2 = System.nanoTime();
+            numbers2.remove(0); //menghapus index yang diinginkan
+            System.out.println("Arraylist setelah penghpausan: " + numbers2);
+            long stopTimeDeletion2 = System.nanoTime();
+            durations[3] = stopTimeDeletion2 - startTimeDeletion2;
+            printOperationTime("delete pada arraylist: ",durations[3]);
 
 
-        //Delete pada Array
-        long startTimeDeletion = System.nanoTime();
-        int[] deleteNumbers = new int[newNumbers.length-1];
-        int indexDeletion = 0;
-        System.arraycopy(newNumbers, 0, deleteNumbers, 0, indexDeletion );
-        System.arraycopy(newNumbers, indexDeletion + 1, deleteNumbers, indexDeletion, newNumbers.length-1-indexDeletion);
-        System.out.println("Array setelah penghapusan: " + Arrays.toString((deleteNumbers)));
-        long stopTimeDeletion = System.nanoTime();
-        long durationDeletion = stopTimeDeletion - startTimeDeletion;
+            //Reverse pada arraylist
+            long startTimeReverse2 = System.nanoTime();
+            Collections.reverse(numbers2); //melakukan reversal
+            System.out.println("Arraylist Reverse: " + numbers2);
+            long stopTimeReverse2 = System.nanoTime();
+            durations[4] = stopTimeReverse2 - startTimeReverse2;
+            printOperationTime("Reverse pada arraylist: ",durations[4]);
 
-
-        // Delete pada arraylist
-        long startTimeDeletion2 = System.nanoTime();
-        numbers2.remove(0);
-        System.out.println("Arraylist setelah penghpausan: " + numbers2 + "\n");
-        long stopTimeDeletion2 = System.nanoTime();
-        long durationDeletion2 = stopTimeDeletion2 - startTimeDeletion2;
-
-
-        //Reverse pada Array
-        int[] reverseNumbers = new int[deleteNumbers.length];
-        for(int i = 0 ; i < deleteNumbers.length; i++){
-            reverseNumbers[i] = deleteNumbers[deleteNumbers.length - 1 - i];
+            return durations;
         }
-        long startTimeReverse = System.nanoTime();
-        System.out.println("Array Reverse: " + Arrays.toString((reverseNumbers)));
-        long stopTimeReverse = System.nanoTime();
-        long durationReverse = stopTimeReverse - startTimeReverse;
+}
 
+class Comparison{
+    public void printComparisonResult(long[] arrayDurations, long[] arraylistDurations) {
+        System.out.println("Waktu eksekusi traversal pada Array: " + ((float) arrayDurations[0] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi pencarian pada Array: " + ((float) arrayDurations[1] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi insertion pada Array: " + ((float) arrayDurations[2] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi deletion pada Array: " + ((float) arrayDurations[3] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi reversal pada Array: " + ((float) arrayDurations[4] / 1e6 + " ms\n"));
 
-        //Reverse pada arraylist
-        long startTimeReverse2 = System.nanoTime();
-        Collections.reverse(numbers2);
-        System.out.println("Arraylist Reverse: " + numbers2 +"\n");
-        long stopTimeReverse2 = System.nanoTime();
-        long durationReverse2 = stopTimeReverse2 - startTimeReverse2;
-
-
-
-        System.out.println("Waktu eksekusi traversal pada Array: " + ((float) durationTraversal/1e6 + " ms"));
-        System.out.println("Waktu eksekusi pencarian pada Array: " + ((float) durationPencarian/1e6 + " ms"));
-        System.out.println("Waktu eksekusi insertion pada Array: " + ((float) durationInsertion/1e6 + " ms"));
-        System.out.println("Waktu eksekusi deletion pada Array: " + ((float) durationDeletion/1e6 + " ms"));
-        System.out.println("Waktu eksekusi reversal pada Array: " + ((float) durationReverse/1e6 + " ms\n"));
-
-
-        System.out.println("Waktu eksekusi traversal pada Arraylist: " + ((float) durationTraversal2/1e6 + " ms"));
-        System.out.println("Waktu eksekusi pencarian pada Arraylist: " + ((float) durationPencarian2/1e6 + " ms"));
-        System.out.println("Waktu eksekusi insertion pada Arraylist: " + ((float) durationInsertion2/1e6 + " ms"));
-        System.out.println("Waktu eksekusi deletion pada Arraylist: " + ((float) durationDeletion2/1e6 + " ms"));
-        System.out.println("Waktu eksekusi reversal pada Arraylist: " + ((float) durationReverse2/1e6 + " ms\n"));
-
+        System.out.println("Waktu eksekusi traversal pada Arraylist: " + ((float) arraylistDurations[0] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi pencarian pada Arraylist: " + ((float) arraylistDurations[1] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi insertion pada Arraylist: " + ((float) arraylistDurations[2] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi deletion pada Arraylist: " + ((float) arraylistDurations[3] / 1e6 + " ms"));
+        System.out.println("Waktu eksekusi reversal pada Arraylist: " + ((float) arraylistDurations[4] / 1e6 + " ms\n"));
     }
 }
